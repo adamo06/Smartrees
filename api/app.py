7d6d013bdfe31c3d44d4f64fe87d40c67bb6fdee
adam bellity <adambellity@yahoo.fr>
2021-09-09 23:04:34 +0200

    Add google credentials

--- api/app.py
+++ api/app.py
@@ -8,50 +8,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 import base64
 
-
 from google.auth import compute_engine
-
 
 import os
 import json
 import ee
 
-# tout ça pour ça
+# Credentials for GEE
 json_str = os.getenv('GOOGLE_APPLICATION_CREDENTIALS42')
 
 json_data = json.loads(json_str)
 json_data['private_key'] = json_data['private_key'].replace('\\n', '\n')
 
-outfile=open('test.json','w')
-json.dump(json_data,outfile,indent=11)
+outfile = open('test.json', 'w')
+json.dump(json_data, outfile, indent=11)
 outfile.close()
 
 service_account = '9319975735-compute@developer.gserviceaccount.com '
-credentials = ee.ServiceAccountCredentials(service_account,'test.json')
+credentials = ee.ServiceAccountCredentials(service_account, 'test.json')
 ee.Initialize(credentials)
 os.remove('test.json')
-
-"""ee_image='LANDSAT/LC08/C01/T1_TOA/LC08_195030_20210729'
-img = ee.Image(ee_image).select(['B10'])
-
-credentials = compute_engine.Credentials(scopes=['https://www.googleapis.com/auth/earthengine'])
-ee.Initialize(credentials)"""
-
-
 
 # alternative icon image
 icon = Image.open("api/imgs/SmarTree.JPG")
 
 st.set_page_config(layout='centered', page_title='SmarTrees', page_icon=icon)
 
-def _max_width_(prcnt_width:int = 25):
+
+def _max_width_(prcnt_width: int = 25):
     max_width_str = f"max-width: {prcnt_width}%;"
-    st.markdown(f"""
+    st.markdown(
+        f"""
                 <style>
                 .reportview-container .main .block-container{{{max_width_str}}}
                 </style>
                 """,
-                unsafe_allow_html=True,
+        unsafe_allow_html=True,
     )
 
 
@@ -65,14 +57,12 @@ h2 {
 h3 {
     color: DarkGreen;
 }
-
 .stApp {
     background-image: url(https://wallpaperaccess.com/full/1429640.jpg);
     background-size: cover;
 }
 """
 st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
-
 
 st.markdown("""
     # SmarTrees
@@ -85,7 +75,6 @@ if st.button('Authentify'):
     #st.echo(ee.Initialize())
 
 st.markdown("""
-
     #### Choose a city and a date, discover in which areas trees are needed the most
     ####
 """)
@@ -94,7 +83,6 @@ st.markdown("""
 pos = st.text_input('Select city coordinates', '7.25, 43.7')
 
 col1, col2 = st.columns(2)
-
 
 # input start date
 date0 = col1.text_input('Beginning date', '2019-07-01')
@@ -110,32 +98,34 @@ import Smartrees.get_dataFrame as smgdf
 import Smartrees.ee_query as smeq
 
 Pos = pos.split(',')
-pos_c=[float(Pos[0]),float(Pos[1])]
-close_date = smeq.closest_image(date0, formatDate = 1, pos= tuple([float(item) for item in Pos]))
+pos_c = [float(Pos[0]), float(Pos[1])]
+close_date = smeq.closest_image(date0,
+                                formatDate=1,
+                                pos=tuple([float(item) for item in Pos]))
 
 #st.write(close_date+timedelta(days=1))
-
 
 # affichage gif dans le site
 import Smartrees.pngs_to_gif as smptg
 import Smartrees.date_to_data as smdtd
 import os
 
-
-
-df = smeq.get_meta_data(date_start  = str(close_date) , date_stop = str(close_date+timedelta(days=1)), pos = tuple([float(item) for item in Pos]))
-st.write('Selected date is ',close_date)
-smart=smgdf.SmarTrees(ee_image=df.loc[0,'id'], pos = tuple([float(item) for item in Pos]))
-df1=smart.get_NDVIandKELVIN()
-minNDVI=df1['NDVI'].min()
-maxNDVI=df1['NDVI'].max()
+df = smeq.get_meta_data(date_start=str(close_date),
+                        date_stop=str(close_date + timedelta(days=1)),
+                        pos=tuple([float(item) for item in Pos]))
+st.write('Selected date is ', close_date)
+smart = smgdf.SmarTrees(ee_image=df.loc[0, 'id'],
+                        pos=tuple([float(item) for item in Pos]))
+df1 = smart.get_NDVIandKELVIN()
+minNDVI = df1['NDVI'].min()
+maxNDVI = df1['NDVI'].max()
 #mintemp=df1['B10'].min()-273.15
 #maxtemp=df1['B10'].max()-273.15
-m = smart.display_folium_map(min_temp=0, max_temp=40, minNDVI=minNDVI, maxNDVI=maxNDVI)
+m = smart.display_folium_map(min_temp=0,
+                             max_temp=40,
+                             minNDVI=minNDVI,
+                             maxNDVI=maxNDVI)
 folium_static(m)
-
-
-
 
 st.markdown("""
     ## Areas of interest
@@ -143,37 +133,41 @@ st.markdown("""
     ###
 """)
 
-col1,col2,col3=st.columns([1,3,1])
+col1, col2, col3 = st.columns([1, 3, 1])
 if st.button('Show'):
-    df,shapes = smart.z_temperature()
+    df, shapes = smart.z_temperature()
+
     def custom_index_ndvi(x, seuil=0.5):
-        if x<0:
+        if x < 0:
             return 0
-        elif x>seuil:
+        elif x > seuil:
             return 0
-        return 1-x
+        return 1 - x
 
     def custom_index_temp(x):
-        if x<0:
+        if x < 0:
             return 0
         return x
-    temp=df['Norm_Temp']
-    ndvi=df['NDVI']
-    index=temp.map(custom_index_temp)*ndvi.map(custom_index_ndvi)
-    Tree_necessity_index=(index+abs(min(index)))/(max(index)-min(index))*100
 
-    def fill_zeros(data=Tree_necessity_index,size=shapes[10][0]*shapes[10][1]):
-        df=pd.DataFrame(data.copy())
-        true_size=len(df)
-        df['indice']=df.index
-        datframe=pd.DataFrame(np.zeros((size,)),columns={'Tree_index'})
-        datframe.loc[df.index,'Tree_index']=df.iloc[:,0]
+    temp = df['Norm_Temp']
+    ndvi = df['NDVI']
+    index = temp.map(custom_index_temp) * ndvi.map(custom_index_ndvi)
+    Tree_necessity_index = (index + abs(min(index))) / (max(index) -
+                                                        min(index)) * 100
+
+    def fill_zeros(data=Tree_necessity_index,
+                   size=shapes[10][0] * shapes[10][1]):
+        df = pd.DataFrame(data.copy())
+        true_size = len(df)
+        df['indice'] = df.index
+        datframe = pd.DataFrame(np.zeros((size, )), columns={'Tree_index'})
+        datframe.loc[df.index, 'Tree_index'] = df.iloc[:, 0]
         return datframe
 
-    Tree_necessity_index_filled =fill_zeros()
+    Tree_necessity_index_filled = fill_zeros()
 
-    fig,ax=plt.subplots(figsize=(5,5))
-    im=np.array(Tree_necessity_index_filled).reshape(shapes[10])
+    fig, ax = plt.subplots(figsize=(5, 5))
+    im = np.array(Tree_necessity_index_filled).reshape(shapes[10])
     s = ax.imshow(im, cmap='viridis')
     ax.set_title('Tree necessity index')
     ax.set_xticks([])
@@ -187,13 +181,12 @@ st.markdown("""
 
 if st.button('Display gif'):
 
-
     file_ = open("api/imgs/NDVI_ok.gif", "rb")
     contents = file_.read()
     data_url = base64.b64encode(contents).decode("utf-8")
     file_.close()
 
-    col5,col6=st.columns([2,2])
+    col5, col6 = st.columns([2, 2])
     col5.markdown("""
     #### Gif of Vegetation Index
     #
